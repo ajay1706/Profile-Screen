920dcace4369586ed7a99981cb4c59e25dc48951
ajay1706 <ajay97456@gmail.com>
2019-10-11 03:15:24 +0530

    added clipper and imag

---
+++ profile_app/lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: HomePage(),
+));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          ClipPath(
+            child: Container(
+              color: Colors.black.withOpacity(0.8),
+            ),
+            clipper: getClipper(),
+          ),
+          Positioned(
+            width: 350,
+            top: MediaQuery.of(context).size.height/5,
+            left: MediaQuery.of(context).size.width/12,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  width: 200,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    boxShadow:[ BoxShadow(
+                     
+                    blurRadius: 7.0,
+                      color: Colors.black
+                    )],     
+                    image: DecorationImage( 
+       image: NetworkImage("https://avatars3.githubusercontent.com/u/20722807?s=460&v=4"),
+                      fit: BoxFit.cover,
+                    ),
+                    borderRadius: BorderRadius.all(Radius.circular(100))
+                  ),
+                ),
+                SizedBox(height: 90,),
+                Text("Ajay Sharma",
+                style: TextStyle(
+                  fontFamily: 'MonteSerrat',
+                  fontSize: 35,
+                  fontWeight: FontWeight.bold
+
+                ),),
+                SizedBox(height: 15,),
+                   Text("Flutter & ML Developer",
+                style: TextStyle(
+                  fontFamily: 'MonteSerrat',
+                  fontSize: 15,
+                  fontStyle: FontStyle.italic
+
+                ),)
+
+              ],
+            ),
+          )
+        ],
+      ),
+      
+    );
+  }
+}
+
+
+class getClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(0.0, size.height/1.9);
+    path.lineTo(size.width + 125, 0.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+
+    return true;
+  }
+
+}
